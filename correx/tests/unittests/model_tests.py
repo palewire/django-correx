c344d78350b84de2d5a17df71d3d13545b5072d9
palewire <ben.welsh@latimes.com>
2009-02-14 23:35:15 -0800

    Added model_test for count_changes()

--- correx/tests/unittests/model_tests.py
+++ correx/tests/unittests/model_tests.py
@@ -1,12 +1,20 @@
-from correx.models import Change
+from correx.models import Change, ChangeType
 from correx.tests import ChangeTestCase
 
 class CorrexModelTests(ChangeTestCase):
 
-    def testSave(self):
-        """ 
-        Save a set of sample changes.
-        """
-        for c in self.createSomeChanges():
-            self.failIfEqual(c.pub_date, None)
-            c.save()
+	def testSave(self):
+		""" 
+		Save a set of sample changes.
+		"""
+		for c in self.createSomeChanges():
+			self.failIfEqual(c.pub_date, None)
+			c.save()
+
+	def testCount(self):
+		"""
+		Tests the count_changes() method invoked by signals.py
+		"""
+		for c in ChangeType.objects.all():
+			c.count_changes()
+			self.assertEquals(c.change_count, c.change_set.filter(is_public=True).count())
