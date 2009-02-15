0cfb18068e5ece3a8fbbd437bced5726a0271ffc
palewire <ben.welsh@latimes.com>
2009-02-15 03:33:41 -0800

    Got the basic cascade from app to model down via a new AJAX view

--- correx/views.py
+++ correx/views.py
@@ -1,1 +1,32 @@
-# Create your views here.
+from django.utils import simplejson
+from django.template import RequestContext
+from django.http import Http404, HttpResponse
+
+from django.contrib.contenttypes.models import ContentType
+
+def filter_contenttypes_by_app(request):
+	""" 
+	Intended to fetch AJAX calls from admin templates.
+	"""
+	# If there is not a POST request throw a 404
+	if not request.GET:
+		raise Http404
+		
+	response_list = [{'Text': '---------', 'Value': ''}]
+	
+	# Grab the post variable
+	app_label = request.GET.get('app_label')
+	if not app_label:
+		HttpResponse(simplejson.dumps(response_dict), mimetype='application/javascript')
+	
+	try:
+		# Fetch all of the ContentType records from that app
+		content_types = ContentType.objects.filter(app_label=app_label)
+	except:
+		raise Http404
+	
+	# Load the results into the dictionary
+	for ct in content_types:
+		response_list += [{'Text': ct.name, 'Value': str(ct.pk)}]
+
+	return HttpResponse(simplejson.dumps(response_list), mimetype='application/javascript')
