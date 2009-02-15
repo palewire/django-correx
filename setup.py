9978502dbd92ceb82ee8e9242f0d55da83d719dc
palewire <ben.welsh@latimes.com>
2009-02-15 13:58:23 -0800

    Tweaked setup.py to made sure it captured the templates and fixtures

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from distutils.core import setup
 
+data = []
+data.extend([
+    'templates/admin/correx/*.html',
+    'fixtures/*.json',
+    'tests/fixtures/*.json'
+    ]
+)
+
 setup(name="django-correx",
       description="A set of models and template tags for pulling in lists of content changes across applications.",
-      version='0.2',
+      version='0.21',
       author='Ben Welsh',
       author_email='palewire@palewire.com',
       url="http://www.palewire.com",
@@ -13,4 +21,5 @@ setup(name="django-correx",
          "correx.tests",
          "correx.tests.fixtures",
          "correx.tests.unittests",],
+     package_data={ 'correx': data },
     )
